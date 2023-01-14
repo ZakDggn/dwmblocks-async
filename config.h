087ffd30229dd3c03eecf4e6a6e735b7a9471a9a
ZakDggn <23458039+ZakDggn@users.noreply.github.com>
2023-01-14 11:16:45 +0000

    Added battery block and volume mute indication

--- config.h
+++ config.h
@@ -1,8 +1,9 @@
 #define CMDLENGTH 45
-#define DELIMITER "  "
+#define DELIMITER " | "
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
 	BLOCK("~/dwmblocks-async/sb-volume",  1,    1),
+	BLOCK("~/dwmblocks-async/sb-battery", 60,   0),
 	BLOCK("~/dwmblocks-async/sb-date",    1,    0)
 };
